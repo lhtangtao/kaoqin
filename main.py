5bed0cc6b1d04138ad9f9e3286958a2419295471
tangtao <tangtao@lhtangtao.com>
2018-02-01 10:39:05 +0800

    现在是2018年了  更新下数据

--- main.py
+++ main.py
@@ -23,10 +23,11 @@ from send_mail import send_email
 
 
 def infos(ID, month, address):
-    start = "2017/" + str(month) + "/1"
-    end = "2017/" + str(month) + "/" + str(calendar.monthrange(2017, month)[1])
+    start = "2018/" + str(month) + "/1"
+    end = "2018/" + str(month) + "/" + str(calendar.monthrange(2018, month)[1])
     init_date = get_info_from_web(ID, start, end)  # 从浏览器中自动获取数据
     after = duplicate_removal(init_date)
+
     info_to_send = overtime_money(after)
     print info_to_send
     # send_email(address, info_to_send)
@@ -37,5 +38,5 @@ if __name__ == '__main__':
     # for i in range(0,len(name_list)):
     #     infos(ID=name_list[i], month=7)
     #     print "   "
-    infos(ID=str(sys.argv[1]), month=int(sys.argv[2]), address=str(sys.argv[3]))
-    # infos(0107063, 9, 'tt')
+    # infos(ID=str(sys.argv[1]), month=int(sys.argv[2]), address=str(sys.argv[3]))
+    infos("0107063", 1, 'tt')
